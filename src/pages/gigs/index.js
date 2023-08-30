035a10f931bd0d00c8b25cbd0aa4ea9010c395d4
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2023-08-29 21:59:55 -0400

    Initial Gig skeleton table and scripts

--- src/pages/gigs/index.js
+++ src/pages/gigs/index.js
@@ -7,11 +7,29 @@ export default function Gigs() {
     <div>
       <Head>
         <title>Soul Harvest - Gigs</title>
-        <Script language="javascript" src="/scripts/popup.js" />
+        <Script language="javascript" src="/scripts/calendar/popup.js" />
+        <Script language="javascript" src="/scripts/calendar/2004/october/gigInfo.js" />
       </Head>
 
       <h1>Gigs</h1>
       <div className={styles.row}>
+        <center>
+          <table border="0" cellPadding="0" cellSpacing="0" width="60%" cols="2">
+            <tbody>
+              <tr>
+                <td>NAV HERE</td>
+                <td align="right">
+                  <div className="month">MONTH TITLE HERE</div>
+                </td>
+              </tr>
+              <tr>
+                <td colSpan="2">
+                  MONTH CALENDAR HERE
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </center>
       </div>
     </div>
   )
