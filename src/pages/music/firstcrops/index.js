d885839353a62861ff0b84f49f7d80680913890a
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2023-09-20 22:46:28 -0400

    Initial version of First Crops landing page

---
+++ src/pages/music/firstcrops/index.js
@@ -0,0 +1,100 @@
+import Head from 'next/head';
+import styles from '../../../styles/soulharvest.module.css';
+
+export default function FirstCrops() {
+  return (
+    <div>
+      <Head>
+        <title>Soul Harvest - First Crops EP</title>
+      </Head>
+    
+      <h1>First Crops EP</h1>
+      <div className={styles.row}>
+        <blockquote>
+          <p>Until the release of the new CD, there will be no radio-release music available from Soul Harvest.  For the time being, however, you can enjoy these sample demos recorded at Bova Sound studios, and mixed by Eric St-Cyr of Auto-Wah Records.</p>
+          <p>Recordings from previous years are available in the archive.</p>
+          <a href="/music/archive">[archive...]</a>
+        </blockquote>
+      </div>
+      <div className={styles.row}>
+        <table border="0" cols="2" cellSpacing="5" cellPadding="5" width="85%">
+          <tbody>
+            <tr valign="top">
+              <td colspan="2">
+                <center>
+                  <br />
+                  <img src="firstcrops/firstcrops.jpg" height="200" />
+                  <br /><br />
+                </center>
+              </td>
+            </tr>
+            <tr valign="top">
+              <td width="50%">
+                <font face="arial,helvetica,verdana" size="-1">
+                  <b>Album: </b>First Crops EP<br />
+                  <b>Recorded: </b>July 1998<br />
+                  <b>Where: </b>Spaz Records, Orleans, ON<br />
+                  <br /><br />
+                
+                  Soul Harvest spent an entire day with an old 80's tape deck, two mikes, and a basement full of gear slamming together as much material as the makeshift group could put together.  At the time of recording, the bassist and drummer were missing, and with Eric St-Cyr filling in, the remaining members recorded as much as possible with as few takes as possible. While the sound quality was quite atrocious, a tracks like "Something Soothing" and "John Doe" showcased what was to come in future years, and the garbled recordings of "Windflow" and "Stumbled" were a fine first attempt at recording these SH rock tunes.
+                  <br /><br />
+                  You can currently purchase the First Crops EP from our <a href="http://www.mp3.com/soulharvest" target="_new">mp3.com website</a>.  Drop us a line and let us know what you think at: <a href="mailto:soulharvest@canada.com">soulharvest@canada.com</a>
+                  <br /><br />
+                </font>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className={styles.row}>
+        <center>
+          <table cols="3" cellPadding="2" cellSpacing="0" border="0" width="85%">
+            <tbody>
+              <tr bgcolor="#444444">
+                <td align="left" width="50%" valign="top">
+                  <font face="arial,helvetica,verdana"  color="#0099FF"><b>SONG</b> <font size="-1">(click for more info)</font></font>
+                </td>
+
+                <td align="left" valign="top">
+                  <center>
+                    <font face="arial,helvetica,verdana"  color="#0099FF"><b>GET</b></font>
+                  </center>
+                </td>
+              </tr>
+
+              <tr valign="middle">
+                <td><img src="firstcrops/firstcrops_s.jpg" alt="First Crops" border="0" height="50" width="50" align="middle" /><a href="lush.html"><font  face="arial,helvetica" size="-1" color="#33CCFF">Lush</font></a></td>
+                <td align="center">&nbsp;<a href="http://www.besonic.com/soulharvest" target="_new"><img src="/graphics/download.gif" height="30" border="0" /></a> </td>
+              </tr>    
+
+              <tr bgcolor="#444444" valign="middle">
+                <td><img src="firstcrops/firstcrops_s.jpg" alt="First Crops" border="0" height="50" width="50" align="middle" /><a href="stumbled.html"><font face="arial,helvetica" size="-1" color="#33CCFF">Stumbled</font></a></td>
+                <td align="center">&nbsp;<a href="http://www.mp3.com/soulharvest" target="_new"><img src="/graphics/download.gif" height="30" border="0" /></a> </td>
+              </tr>
+
+              <tr valign="middle">
+                <td><img src="firstcrops/firstcrops_s.jpg" alt="First Crops" border="0" height="50" width="50" align="middle" /><a href="soothing.html"><font  face="arial,helvetica" size="-1" color="#33CCFF">Something Soothing</font></a></td>
+                <td align="center"><a href="http://www.mp3.com/soulharvest" target="_new"><img src="/graphics/download.gif" height="30" border="0" /></a></td>
+              </tr>
+
+              <tr bgcolor="#444444" valign="middle">
+                <td><img src="firstcrops/firstcrops_s.jpg" alt="First Crops" border="0" height="50" width="50" align="middle" /><a href="chilling.html"><font  face="arial,helvetica" size="-1" color="#33CCFF">Chillin' on the Patio</font></a></td>
+                <td align="center">&nbsp;<a href="http://www.besonic.com/soulharvest" target="_new"><img src="/graphics/download.gif" height="30" border="0" /></a> </td>
+              </tr>
+
+              <tr valign="middle">
+                <td><img src="firstcrops/firstcrops_s.jpg" alt="First Crops" border="0" height="50" width="50" align="middle" /><a href="windflow.html"><font  face="arial,helvetica" size="-1" color="#33CCFF">Windflow</font></a></td>
+                <td align="center">&nbsp;<a href="http://www.mp3.com/soulharvest" target="_new"><img src="/graphics/download.gif" height="30" border="0" /></a> </td>
+              </tr>
+
+              <tr bgcolor="#444444" valign="middle">
+                <td><img src="firstcrops/firstcrops_s.jpg" alt="First Crops" border="0" height="50" width="50" align="middle" /><a href="johndoe.html"><font  face="arial,helvetica" size="-1" color="#33CCFF">John Doe</font></a></td>
+                <td align="center"><a href="http://www.mp3.com/soulharvest" target="_new"><img src="/graphics/download.gif" height="30" border="0" /></a></td>
+              </tr>
+            </tbody>
+          </table>
+        </center>
+      </div>       
+    </div>
+  )
+}
