0d6b3fdb0c7283f8baae789eda22a785d2a1538c
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2023-09-24 18:14:56 -0400

    Initial translation of markup

--- src/pages/music/firstcrops/chilling.js
+++ src/pages/music/firstcrops/chilling.js
@@ -10,6 +10,64 @@ export default function ChillingOnThePatio() {
     
       <h1>Chillin' on the Patio (First Crops)</h1>
       <div className={styles.row}>
+        <table>
+          <tbody>
+            <tr valign="top">
+              <td width="50%">
+                <font face="arial,helvetica,verdana" size="-1">
+                  <img src="/music/firstcrops/firstcrops.jpg" width="150" height="150" />
+                  <br />
+                  <b>Album: </b>The First Crops EP<br />
+                  <b>Lyrics by: </b>Sean McCullough and Jason St-Cyr<br />
+                  <b>Music by: </b>Sean McCullough and Jason St-Cyr<br />
+                  <b>Performed by: </b>Soul Harvest<br />
+                  <b>Recorded: </b>Summer 1998 in Orleans, Ontario<br /><br />
+
+
+                  <table border="0">
+                    <tbody>
+                      <tr valign="top">
+                        <td>
+                          <a href="/music/firstcrops/chilling_clip.mp3"><img src="/graphics/spkr.gif" border="0" /></a>
+                        </td>
+                        <td>
+                          <font face="arial,helvetica,verdana" size="-1">Clip</font>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img src="/graphics/download.gif" height="20" border="0" />
+                        </td>
+                        <td>
+                          <font face="arial,helvetica,verdana" size="-1">Download [N/A]</font>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <a href="/music/firstcrops/"><img src="/music/firstcrops/firstcrops_s.jpg" height="20" border="0" /></a>
+                        </td>
+                        <td>
+                          <font face="arial,helvetica,verdana" size="-1">CD INFO</font>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </font>
+              </td>
+              <td>
+                <font face="arial,helvetica,verdana" size="-1">
+                  <font size="+1"><b>Lyrics and Story</b></font>
+                  <br /><br />
+                  "Sorry ma'am, we're not stoned<br />
+                  We're like this anyways"<br /><br />
+                  This song has become like a flagship for the Soul Harvest set. For whatever reason, every member of the audience seems to enjoy this one. Most state that it's because they can relate to what's going on, even though the entire song revolves around inside jokes known only to a few people. <br /><br />
+                  Lines like "To the drive-ins and the praying mantis" mean absolutely nothing to anyone hearing it, but the up-beat feel of the whole song, mixed with the laid-back approach the band takes to performing this track gets everybody into it. <br /><br />
+                  This recording was done in Summer 1998, and was re-recorded for "The Rift" and the upcoming SH album due out by the end of 2000.
+                </font>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>       
     </div>
   )
