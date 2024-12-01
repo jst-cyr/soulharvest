222c9d6fd53a2fd02a269140b081a03e96ff35a7
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2024-12-01 10:33:16 -0500

    Make houses look like houses

--- htdocs/paperboy/js/houses.js
+++ htdocs/paperboy/js/houses.js
@@ -32,11 +32,38 @@ export function drawHouses(ctx, canvas, houses, houseCount, maxHouses, gameState
     }
 
     houses.forEach((house) => {
+        // Draw the house body
         ctx.fillStyle = house.color;
         ctx.fillRect(house.x, house.y, house.width, house.height);
 
+        // Draw the roof
+        ctx.fillStyle = "darkgray";
+        ctx.beginPath();
+        ctx.moveTo(house.x, house.y); // Left corner of the roof
+        ctx.lineTo(house.x + house.width / 2, house.y - 50); // Peak of the roof
+        ctx.lineTo(house.x + house.width, house.y); // Right corner of the roof
+        ctx.closePath();
+        ctx.fill();
+
+        // Draw the door
+        ctx.fillStyle = "brown";
+        const doorWidth = house.width / 4;
+        const doorHeight = house.height / 3;
+        ctx.fillRect(house.x + house.width / 2 - doorWidth / 2, house.y + house.height - doorHeight, doorWidth, doorHeight);
+
+        // Draw windows
+        ctx.fillStyle = "lightblue";
+        const windowSize = house.width / 5;
+        ctx.fillRect(house.x + 10, house.y + 30, windowSize, windowSize); // Left window
+        ctx.fillRect(house.x + house.width - windowSize - 10, house.y + 30, windowSize, windowSize); // Right window
+
+        // Draw the mailbox
         const mailbox = house.mailbox;
         ctx.fillStyle = mailbox.delivered ? "gray" : mailbox.color;
-        ctx.fillRect(mailbox.x, mailbox.y, mailbox.width, mailbox.height);
+        ctx.fillRect(mailbox.x, mailbox.y, mailbox.width, mailbox.height); // Mailbox body
+
+        // Draw mailbox post
+        ctx.fillStyle = "black";
+        ctx.fillRect(mailbox.x + mailbox.width / 2 - 2, mailbox.y + mailbox.height, 4, 20); // Mailbox post
     });
 }
