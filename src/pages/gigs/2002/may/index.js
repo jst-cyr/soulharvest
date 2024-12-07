cb6e9ff4be845eed0d933ca9589be0455ee59672
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2024-12-07 11:24:16 -0500

    Migrating May 2002 into gigs

--- src/pages/gigs/2002/may/index.js
+++ src/pages/gigs/2002/may/index.js
@@ -16,9 +16,9 @@ export default function Gigs() {
           <table border="0" cellPadding="0" cellSpacing="0" width="60%" cols="2">
             <tbody>
               <tr>
-                <td><div className={calendarStyles.dates}><b><a href="/gigs/2002/june/">JUNE</a> | <a href="/gigs/2003/march/">2003</a></b></div></td>
+                <td><div className={calendarStyles.dates}><b><a href="/gigs/2002/march/">MARCH</a> | <a href="/gigs/2002/june/">JUNE</a></b></div></td>
                 <td align="right">
-                  <div className={calendarStyles.month}>August 2002</div>
+                  <div className={calendarStyles.month}>May 2002</div>
                 </td>
               </tr>
               <tr>
@@ -47,61 +47,56 @@ export default function Gigs() {
                       <td bgcolor="#444444">&nbsp;<br /><br /><br /></td>
                       <td bgcolor="#444444">&nbsp;</td>
                       <td bgcolor="#444444">&nbsp;</td>
-                      <td bgcolor="#444444">&nbsp;</td>
                       <td> 1</td>
-                      <td bgcolor="#76A7CB" onClick={() => gigInfo('/gigs/2002/august/2.html')}>
-                        <a><b>2<br />
-                        Underground<br />
-			                  9:30pm</b></a><br />
-			                  <br />
-                      </td>
+                      <td> 2</td>
                       <td> 3</td>
+                      <td> 4</td>
                     </tr>
                     <tr valign="top" align="center">
-                      <td> 4<br /></td>
-                      <td> 5</td>
+                      <td> 5<br /></td>
                       <td> 6</td>
                       <td> 7</td>
                       <td> 8</td>
-                      <td> 9</td>
-                      <td> 10</td>
-                    </tr>
-                    <tr valign="top" align="center">
-                      <td> 11<br /><br /><br /></td>
-                      <td> 12</td>
-                      <td> 13</td>
-                      <td> 14</td>
-                      <td> 15</td>
-                      <td bgcolor="#76A7CB" onClick={() => gigInfo('/gigs/2002/august/16.html')}>
-                        <a><b>16<br />
-                        Bumpers<br />
+                      <td bgcolor="#76A7CB" onClick={() => gigInfo('/gigs/2002/may/9.html')}>
+                        <a><b>9<br />
+                        Café Dekcuf<br />
 			                  9:30pm</b></a><br />
 			                  <br />
                       </td>
+                      <td> 10</td>
+                      <td> 11</td>
+                    </tr>
+                    <tr valign="top" align="center">
+                      <td> 12<br /><br /><br /></td>
+                      <td> 13</td>
+                      <td> 14</td>
+                      <td> 15</td>
+                      <td> 16</td>
                       <td> 17</td>
+                      <td> 18</td>
                     </tr>
                     <tr valign="top" align="center"> 
-                      <td> 18<br /><br /><br /></td>
-                      <td> 19</td>
+                      <td> 19<br /><br /><br /></td>
                       <td> 20</td>
                       <td> 21</td>
                       <td> 22</td>
                       <td> 23</td>
                       <td> 24</td>
+                      <td> 25</td>
                     </tr>
                     <tr valign="top" align="center"> 
-                      <td> 25<br /><br /><br /></td>
-                      <td> 26</td>
+                      <td> 26<br /><br /><br /></td>
                       <td> 27</td>
                       <td> 28</td>
                       <td> 29</td>
                       <td> 30</td>
-                      <td bgcolor="#76A7CB" onClick={() => gigInfo('/gigs/2002/august/31.html')}>
+                      <td bgcolor="#76A7CB" onClick={() => gigInfo('/gigs/2002/may/31.html')}>
                         <a><b>31<br />
-                        Babylon<br />
+                        Bumpers<br />
 			                  9:30pm</b></a><br />
 			                  <br />
                       </td>
+                      <td bgcolor="#444444">&nbsp;</td>
                     </tr>
                     </tbody>
                   </table> 
