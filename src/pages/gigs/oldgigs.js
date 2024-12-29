bd0d085b37c47268b1464710489670b78eab46cb
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2024-12-29 00:29:03 -0500

    Adding old gig archive
    Merged stored old gigs with online archive from FortuneCity that has even older shows

---
+++ src/pages/gigs/oldgigs.js
@@ -0,0 +1,138 @@
+import Head from 'next/head';
+import styles from '../../styles/soulharvest.module.css';
+
+export default function OldGigs() {
+  return (
+    <div>
+      <Head>
+        <title>Soul Harvest - Gigs</title>
+      </Head>
+
+      <div id="archived-gigs">
+        <table id="content" border="0" cellPadding="5" cellSpacing="0">
+          <tbody>
+            <tr>
+              <td width="150" valign="top" bgcolor="#444444"><b>January 25 '01</b></td>
+              <td bgcolor="#444444"><b>SH Live!</b><br />
+                Great music, good times, SH @ the CCP!!<br />
+                CRIMSON CLOVER PUB (221 Rideau St.)<br />
+                FREE admission, show starts at 9:30pm
+              </td>
+            </tr>
+            <tr>
+              <td width="150" valign="top"><b>January 11 '01</b></td>
+              <td><b>SH @ CCP!</b><br />
+                The year starts anew, SH @ the CCP!!<br />
+                CRIMSON CLOVER PUB (221 Rideau St.)<br />
+                FREE admission, show starts at 9:30pm
+              </td>
+            </tr>
+            <tr>
+              <td width="150" valign="top" bgcolor="#444444"><b>December 28 '00</b></td>
+              <td bgcolor="#444444"><b>Countdown to 2001!</b><br />
+                Ring in the New Year with SH @ the CCP!!<br />
+                CRIMSON CLOVER PUB (221 Rideau St.)<br />
+                FREE admission, show starts at 9:30pm
+              </td>
+            </tr>
+            <tr>
+              <td width="150" valign="top"><b>December 14 '00</b></td>
+              <td><b>Deck the halls!</b><br />
+                Come see Soul Harvest celebrate the season at the CCP!!<br />
+                CRIMSON CLOVER PUB (221 Rideau St.)<br />
+                FREE admission, show starts at 9:30pm
+              </td>
+            </tr>
+            <tr>
+              <td width="150" valign="top" bgcolor="#444444"><b>November 30 '00</b></td>
+              <td bgcolor="#444444"><b>The House Party continues!</b><br />
+                Come see Soul Harvest as the House Band for the CCP!!<br />
+                CRIMSON CLOVER PUB (221 Rideau St.)<br />
+                FREE admission, show starts at 9:30pm
+              </td>
+            </tr>
+            <tr>
+              <td width="150" valign="top"><b>November 16 '00</b></td>
+              <td><b>Kickin' off the House Party!</b><br />
+                We're kickin' off our bi-monthly appearances as House Band for the CCP!!<br />
+                CRIMSON CLOVER PUB (221 Rideau St.)<br />
+                FREE admission, show starts at 9:30pm
+              </td>
+            </tr>
+            <tr>
+              <td width="150" valign="top" bgcolor="#444444"><b>October 25 '00</b></td>
+              <td bgcolor="#444444">"Battle of the Bands"<br />
+                CRIMSON CLOVER PUB (221 Rideau St.)<br />
+                FREE admission, show starts at 9:30pm
+              </td>
+            </tr>
+            <tr>
+              <td width="150" valign="top"><b>August 6 '00</b></td>
+              <td>"MacFEST" benefit concert (<a href="/macfest/">check out the details!</a>)</td>
+            </tr>
+            <tr>
+              <td valign="top" bgcolor="#444444"><b>June 26, '00</b></td>
+              <td bgcolor="#444444">ZAPHOD BEEBLEBROX 1 (27 York St.) 5$, show starts at 9pm
+                <br /><img src="/graphics/loosecolours.gif" alt="Loose Colours" height="20" width="126" border="0" /> & Soul Harvest
+              </td>
+            </tr>
+            <tr>
+              <td valign="top"><b>April 6, '00</b></td>
+              <td>ZAPHOD BEEBLEBROX 1 (27 York St.) 3$, show starts at 9pm
+                <br /><a href="http://www.circuit.ca">Circuit</a> & Soul Harvest
+              </td>
+            </tr>
+            <tr>
+              <td valign="top" bgcolor="#444444"><b>Jan - Feb. '00</b></td>
+              <td bgcolor="#444444">Open stage appearances at the Rainbow</td>
+            </tr>
+            <tr>
+              <td valign="top"><b>October 31 '99</b></td>
+              <td>"The Final Jam" - Sean McCullough and Dave Van Soest say their goodbyes</td>
+            </tr>
+            <tr>
+              <td valign="top" bgcolor="#444444"><b>October 7, '99</b></td>
+              <td bgcolor="#444444">ZAPHOD BEEBLEBROX 1 (27 York St.) 2$, show starts at 9pm
+                <br /><a href="http://www.geocities.com/SunsetStrip/Palms/7500/">Plastic Soul</a> & Soul Harvest
+              </td>
+            </tr>
+            <tr>
+              <td valign="top"><b>August 26, '99</b></td>
+              <td>ZAPHOD BEEBLEBROX 1 (27 York St.)
+                <br /><img src="/graphics/loosecolours.gif" alt="Loose Colours" height="20" width="126" border="0" /> & Soul Harvest
+              </td>
+            </tr>
+            <tr>
+              <td valign="top" bgcolor="#444444"><b>June 13, '99</b></td>
+              <td bgcolor="#444444">Gloucester Fair - Rockathon competitors</td>
+            </tr>
+            <tr>
+              <td valign="top"><b>April 6, '99</b></td>
+              <td>Whipping Post - open stage performance</td>
+            </tr>
+            <tr>
+              <td valign="top" bgcolor="#444444"><b>March 30, '99</b></td>
+              <td bgcolor="#444444">Whipping Post - open stage performance</td>
+            </tr>
+            <tr>
+              <td valign="top"><b>Sept.-Dec., '98</b></td>
+              <td>Bi-weekly open-stage appearances at the Mad Cow, the Rainbow, the Whipping Post and others</td>
+            </tr>
+            <tr>
+              <td valign="top" bgcolor="#444444"><b>August, '98</b></td>
+              <td bgcolor="#444444">
+                Live entertainment for Mason Proulx's patio party<br />
+                Live entertainment for John Richardson's WSS and Friends party<br />
+                The Ex - Live in the Cow Palace. (Talent competition)<br />
+              </td>
+            </tr>
+            <tr>
+              <td valign="top"><b>July '98</b></td>
+              <td>Basement Blues (first live performance)</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
