81662986fc4213ce47e33636809d416e7614f01e
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2025-01-01 18:53:36 -0500

    Create page for Dekcuf pictures (April 2004)

---
+++ src/pages/pics/2004/2004-04-05/index.js
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+import styles from '../../../../styles/soulharvest.module.css';
+
+export default function Pics() {
+  return (
+    <div>
+      <Head>
+        <title>Soul Harvest - 2004-Apr-05 - Dekcuf</title>
+      </Head>
+    
+      <h1>Soul Harvest - 2004-Apr-05 - Dekcuf</h1>
+      <div className={styles.row}>
+        <table border="0" cellSpacing="0" cellPadding="5"> 
+          <tbody>
+            <tr> 
+              <td><a href="/pics/2004/2004-04-05/2004-04-05_Marc_Ryan_Eric.jpg"><img src="/pics/2004/2004-04-05/2004-04-05_Marc_Ryan_Eric_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-04-05/2004-04-05_Band.jpg"><img src="/pics/2004/2004-04-05/2004-04-05_Band_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-04-05/2004-04-05_Marc_Ryan_Jay.jpg"><img src="/pics/2004/2004-04-05/2004-04-05_Marc_Ryan_Jay_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-04-05/2004-04-05_Chris.jpg"><img src="/pics/2004/2004-04-05/2004-04-05_Chris_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-04-05/2004-04-05_Ryan_Marc.jpg"><img src="/pics/2004/2004-04-05/2004-04-05_Ryan_Marc_s.jpg" border="0" /></a></td> 
+            </tr> 
+            <tr>
+              <td colSpan="5"><b>*Photos courtesy of Lisa Kelly</b></td>
+            </tr>
+          </tbody>
+        </table> 
+
+      </div>
+    </div>
+  )
+}
