104a5c6102e548d01fdc11e02e1bc24e98d27e15
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2025-01-01 19:07:13 -0500

    Adding 2004-02-29 gig pics

---
+++ src/pages/pics/2004/2004-02-29.js
@@ -0,0 +1,47 @@
+import Head from 'next/head';
+import styles from '../../../styles/soulharvest.module.css';
+
+export default function Pics20040229() {
+  return (
+    <div>
+      <Head>
+        <title>Soul Harvest - 2004-Feb-29 - Babylon</title>
+      </Head>
+    
+      <h1>2004-Feb-29 - Babylon</h1>
+      <div className={styles.row}>
+        <table border="0" cellSpacing="0" cellPadding="5"> 
+          <tbody>
+            <tr> 
+              <td><a href="/pics/2004/2004-02-29/2004-02-29_Marc.jpg"><img src="/pics/2004/2004-02-29/2004-02-29_Marc_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-29/2004-02-29_Band.jpg"><img src="/pics/2004/2004-02-29/2004-02-29_Band_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-29/2004-02-29_Jay_Ryan.jpg"><img src="/pics/2004/2004-02-29/2004-02-29_Jay_Ryan_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-29/2004-02-29_Marc2.jpg"><img src="/pics/2004/2004-02-29/2004-02-29_Marc2_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-29/2004-02-29_Jay.jpg"><img src="/pics/2004/2004-02-29/2004-02-29_Jay_s.jpg" border="0" /></a></td> 
+            </tr> 
+
+            <tr> 
+              <td><a href="/pics/2004/2004-02-29/2004-02-29_Band2.jpg"><img src="/pics/2004/2004-02-29/2004-02-29_Band2_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-29/2004-02-29_Ryan.jpg"><img src="/pics/2004/2004-02-29/2004-02-29_Ryan_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-29/2004-02-29_Ryan2.jpg"><img src="/pics/2004/2004-02-29/2004-02-29_Ryan2_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-29/2004-02-29_Chris.jpg"><img src="/pics/2004/2004-02-29/2004-02-29_Chris_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-29/2004-02-29_Ryan_Marc.jpg"><img src="/pics/2004/2004-02-29/2004-02-29_Ryan_Marc_s.jpg" border="0" /></a></td> 
+            </tr> 
+            <tr> 
+              <td><a href="/pics/2004/2004-02-29/2004-02-29_Jay_Ryan_Marc.jpg"><img src="/pics/2004/2004-02-29/2004-02-29_Jay_Ryan_Marc_s.jpg" border="0" /></a></td> 
+              <td>&nbsp;</td> 
+              <td>&nbsp;</td> 
+              <td>&nbsp;</td> 
+              <td>&nbsp;</td> 
+            </tr> 
+
+            <tr>
+              <td colspan="5"><b>*Photos courtesy of Lisa Kelly</b></td>
+            </tr>
+          </tbody>
+        </table> 
+
+      </div>
+    </div>
+  )
+}
