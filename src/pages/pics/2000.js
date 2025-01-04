7186b692ad2c7ad8001abaad065f03a412fd0ecc
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2025-01-04 09:24:17 -0500

    Adding 2000 photo shoot

---
+++ src/pages/pics/2000.js
@@ -0,0 +1,47 @@
+import Head from 'next/head';
+import styles from '../../styles/soulharvest.module.css';
+
+export default function Pics2000() {
+  return (
+    <div>
+      <Head>
+        <title>Soul Harvest - 2000</title>
+      </Head>
+    
+      <h1>2000</h1>
+      <div className={styles.row}>
+        <table border="0" cellSpacing="0" cellPadding="5"> 
+          <tbody>
+            <tr>
+              <td><a href="/pics/2000/zaphods1.jpg"><img src="/pics/2000/zaphods1_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/2000/Kristen.jpg"><img src="/pics/2000/Kristen_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/2000/Chris2000a.jpg"><img src="/pics/2000/Chris2000a_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/2000/Ryan-shed.jpg"><img src="/pics/2000/Ryan-shed_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/2000/band_car.jpg"><img src="/pics/2000/band_car_s.jpg" border="0" width="100" height="100" /></a></td>
+            </tr>
+            <tr>
+              <td><a href="/pics/2000/Band-Line_Jason.jpg"><img src="/pics/2000/Band-Line_Jason_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/2000/John-bubbles.jpg"><img src="/pics/2000/John-bubbles_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/2000/Eric@bbq.jpg"><img src="/pics/2000/Eric@bbq_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/2000/Jason-serious.jpg"><img src="/pics/2000/Jason-serious_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/2000/Kristen-pointing.jpg"><img src="/pics/2000/Kristen-pointing_s.jpg" border="0" width="100" height="100" /></a></td>
+            </tr>
+            <tr>
+              <td><a href="/pics/2000/Eric.jpg"><img src="/pics/2000/Eric_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/2000/Jason-smiling.jpg"><img src="/pics/2000/Jason-smiling_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/2000/Chris2000b.jpg"><img src="/pics/2000/Chris2000b_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/2000/zaphods2.jpg"><img src="/pics/2000/zaphods2_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/2000/Ryan1.jpg"><img src="/pics/2000/Ryan1_s.jpg" border="0" width="100" height="100" /></a></td>
+            </tr>
+            <tr>
+              <td><a href="/pics/2000/John-serious.jpg"><img src="/pics/2000/John-serious_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/2000/Band-Beatles.jpg"><img src="/pics/2000/Band-Beatles_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/2000/Band-Car3.jpg"><img src="/pics/2000/Band-Car3_s.jpg" border="0" width="100" height="100" /></a></td>
+            </tr>
+          </tbody>
+        </table> 
+
+      </div>
+    </div>
+  )
+}
