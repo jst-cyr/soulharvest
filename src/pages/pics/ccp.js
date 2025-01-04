01691e5e249e30fe8c66ffa7f165bc0d7d93459f
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2025-01-04 09:13:59 -0500

    Adding photos from Crimson Clover Pub

---
+++ src/pages/pics/ccp.js
@@ -0,0 +1,65 @@
+import Head from 'next/head';
+import styles from '../../styles/soulharvest.module.css';
+
+export default function Pics2003() {
+  return (
+    <div>
+      <Head>
+        <title>Soul Harvest - Live at the Crimson Clover</title>
+      </Head>
+    
+      <h1>Live at the Crimson Clover</h1>
+      <div className={styles.row}>
+        <table border="0" cellSpacing="0" cellPadding="5"> 
+          <tbody>
+            <tr>
+              <td><a href="/pics/ccp/16-nov-00_2.jpg"><img src="/pics/ccp/16-nov-00_2_s.jpg"  border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/ccp/16-nov-00_3.jpg"><img src="/pics/ccp/16-nov-00_3_s.jpg"  border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/ccp/28-dec-00_10.jpg"><img src="/pics/ccp/28-dec-00_10_s.jpg"  border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/ccp/30-nov-00.jpg"><img src="/pics/ccp/30-nov-00_s.jpg"  border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/ccp/28-dec-00_13.jpg"><img src="/pics/ccp/28-dec-00_13_s.jpg"  border="0" width="100" height="100" /></a></td>
+            </tr>
+
+            <tr>
+              <td><a href="/pics/ccp/16-nov-00.jpg"><img src="/pics/ccp/16-nov-00_s.jpg"  border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/ccp/band_ccp_06.jpg"><img src="/pics/ccp/band_ccp_06_s.jpg"  border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/ccp/28-dec-00_3.jpg"><img src="/pics/ccp/28-dec-00_3_s.jpg"  border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/ccp/22-feb-01.jpg"><img src="/pics/ccp/22-feb-01_s.jpg"  border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/ccp/28-dec-00_11.jpg"><img src="/pics/ccp/28-dec-00_11_s.jpg"  border="0" width="100" height="100" /></a></td>
+            </tr>
+
+            <tr>
+              <td><a href="/pics/ccp/25-oct-00.jpg"><img src="/pics/ccp/25-oct-00_s.jpg"  border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/ccp/28-dec-00_12.jpg"><img src="/pics/ccp/28-dec-00_12_s.jpg"  border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/ccp/band_ccp_07.jpg"><img src="/pics/ccp/band_ccp_07_s.jpg"  border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/ccp/28-dec-00_2.jpg"><img src="/pics/ccp/28-dec-00_2_s.jpg"  border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/ccp/band_ccp_04.jpg"><img src="/pics/ccp/band_ccp_04_s.jpg"  border="0" width="100" height="100" /></a></td>
+            </tr>
+
+            <tr>
+              <td><a href="/pics/ccp/28-dec-00_5.jpg"><img src="/pics/ccp/28-dec-00_5_s.jpg"  border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/ccp/band_ccp_01.jpg"><img src="/pics/ccp/band_ccp_01_s.jpg"  border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/ccp/28-dec-00_7.jpg"><img src="/pics/ccp/28-dec-00_7_s.jpg"  border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/ccp/28-dec-00_8.jpg"><img src="/pics/ccp/28-dec-00_8_s.jpg"  border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/ccp/28-dec-00_9.jpg"><img src="/pics/ccp/28-dec-00_9_s.jpg"  border="0" width="100" height="100" /></a></td>
+            </tr>
+
+            <tr>
+              <td><a href="/pics/ccp/band_ccp_02.jpg"><img src="/pics/ccp/band_ccp_02_s.jpg"  border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/ccp/28-dec-00_6.jpg"><img src="/pics/ccp/28-dec-00_6_s.jpg"  border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/ccp/band_ccp_03.jpg"><img src="/pics/ccp/band_ccp_03_s.jpg"  border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/ccp/28-dec-00.jpg"><img src="/pics/ccp/28-dec-00_s.jpg"  border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/ccp/band_ccp_05.jpg"><img src="/pics/ccp/band_ccp_05_s.jpg"  border="0" width="100" height="100" /></a></td>
+            </tr>
+
+            <tr>
+              <td><a href="/pics/ccp/30-nov-00_2.jpg"><img src="/pics/ccp/30-nov-00_2_s.jpg"  border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/ccp/28-dec-00_4.jpg"><img src="/pics/ccp/28-dec-00_4_s.jpg"  border="0" width="100" height="100" /></a></td>
+            </tr>
+          </tbody>
+        </table> 
+
+      </div>
+    </div>
+  )
+}
