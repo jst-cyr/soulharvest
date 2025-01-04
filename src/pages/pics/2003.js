1799018fc4d0380db16654bb524dd14dd43c242c
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2025-01-04 09:08:00 -0500

    Adding 2003 pics

---
+++ src/pages/pics/2003.js
@@ -0,0 +1,98 @@
+import Head from 'next/head';
+import styles from '../../styles/soulharvest.module.css';
+
+export default function Pics2003() {
+  return (
+    <div>
+      <Head>
+        <title>Soul Harvest - 2003</title>
+      </Head>
+    
+      <h1>2003</h1>
+      <div className={styles.row}>
+        <table border="0" cellSpacing="0" cellPadding="5"> 
+          <tbody>
+          <tr>
+							<td colSpan="5">
+								<h4>Café Deckuf [2003-11-06]</h4>
+							</td>
+						</tr>
+						<tr>
+							<td><a href="/pics/2003/Dekcuf2003-Jay&Chris&Ryan.jpg"><img src="/pics/2003/Dekcuf2003-Jay&Chris&Ryan_s.jpg" border="0" width="100" height="100"/></a></td>
+							<td><a href="/pics/2003/Dekcuf2003-Chris.jpg"><img src="/pics/2003/Dekcuf2003-Chris_s.jpg" border="0" width="100" height="100"/></a></td>
+							<td><a href="/pics/2003/Dekcuf2003-Marc.jpg"><img src="/pics/2003/Dekcuf2003-Marc_s.jpg" border="0" width="100" height="100"/></a></td>
+							<td><a href="/pics/2003/Dekcuf2003-Ryan,Chris&Eric.jpg"><img src="/pics/2003/Dekcuf2003-Ryan,Chris&Eric_s.jpg" border="0" width="100" height="100"/></a></td>
+							<td><a href="/pics/2003/Dekcuf2003-Jay.jpg"><img src="/pics/2003/Dekcuf2003-Jay_s.jpg" border="0" width="100" height="100"/></a></td>
+						</tr>
+
+						<tr>
+							<td colSpan="5"><b>*Photos courtesy of Lisa Kelly</b></td>
+						</tr>
+						<tr>
+							<td colSpan="5"><hr /><br /><br /></td>
+						</tr>
+						<tr>
+							<td colSpan="5"><h4>Bumpers Roadhouse #2 [2003-09-25]</h4></td>
+						</tr>
+						<tr>
+							<td><a href="/pics/2003/Bumpers2003-Band.jpg"><img src="/pics/2003/Bumpers2003-Band_s.jpg" border="0" width="100" height="100"/></a></td>
+							<td><a href="/pics/2003/Bumpers2003-Jay.jpg"><img src="/pics/2003/Bumpers2003-Jay_s.jpg" border="0" width="100" height="100"/></a></td>
+							<td><a href="/pics/2003/Bumpers2003-Chris.jpg"><img src="/pics/2003/Bumpers2003-Chris_s.jpg" border="0" width="100" height="100"/></a></td>
+							<td><a href="/pics/2003/Bumpers2003-Marc.jpg"><img src="/pics/2003/Bumpers2003-Marc_s.jpg" border="0" width="100" height="100"/></a></td>
+							<td><a href="/pics/2003/Bumpers2003-Band+Eric.jpg"><img src="/pics/2003/Bumpers2003-Band+Eric_s.jpg" border="0" width="100" height="100"/></a></td>
+						</tr>
+						<tr>
+							<td><a href="/pics/2003/Bumpers2003-Jay&Ryan.jpg"><img src="/pics/2003/Bumpers2003-Jay&Ryan_s.jpg" border="0" width="100" height="100"/></a></td>
+							<td><a href="/pics/2003/Bumpers2003-Marc2.jpg"><img src="/pics/2003/Bumpers2003-Marc2_s.jpg" border="0" width="100" height="100"/></a></td>
+							<td>&nbsp;</td>
+							<td>&nbsp;</td>
+							<td>&nbsp;</td>
+						</tr>
+						<tr>
+							<td colSpan="5"><b>*Photos courtesy of Lisa Kelly</b></td>
+						</tr>
+						<tr>
+							<td colSpan="5"><hr /><br /><br /></td>
+						</tr>
+						<tr>
+							<td colSpan="5"><h4>Bumpers Roadhouse [2003-09-25]</h4></td>
+						</tr>
+						<tr>
+							<td><a href="/pics/2003/Bumpers_2003-09-25_01.jpg"><img src="/pics/2003/Bumpers_2003-09-25_01_s.jpg" border="0" width="100" height="100"/></a></td>
+							<td><a href="/pics/2003/Bumpers_2003-09-25_02.jpg"><img src="/pics/2003/Bumpers_2003-09-25_02_s.jpg" border="0" width="100" height="100"/></a></td>
+							<td><a href="/pics/2003/Bumpers_2003-09-25_03.jpg"><img src="/pics/2003/Bumpers_2003-09-25_03_s.jpg" border="0" width="100" height="100"/></a></td>
+							<td><a href="/pics/2003/Bumpers_2003-09-25_04.jpg"><img src="/pics/2003/Bumpers_2003-09-25_04_s.jpg" border="0" width="100" height="100"/></a></td>
+							<td><a href="/pics/2003/Bumpers_2003-09-25_05.jpg"><img src="/pics/2003/Bumpers_2003-09-25_05_s.jpg" border="0" width="100" height="100"/></a></td>
+						</tr>
+						<tr>
+							<td colSpan="5"><b>*Photos courtesy of Karen Turnbull</b></td>
+						</tr>
+						<tr>
+							<td colSpan="5"><hr /><br /><br /></td>
+						</tr>
+						<tr>
+							<td colSpan="5">
+								<h4>A Lazy Day at Practice</h4>
+							</td>
+						</tr>
+						<tr>
+							<td><a href="/pics/2003/band_practice1.jpg"><img src="/pics/2003/band_practice1_s.jpg" border="0" width="100" height="100"/></a></td>
+							<td><a href="/pics/2003/jay_smile.jpg"><img src="/pics/2003/jay_smile_s.jpg" border="0" width="100" height="100"/></a></td>
+							<td><a href="/pics/2003/marc_bw.jpg"><img src="/pics/2003/marc_bw_s.jpg" border="0" width="100" height="100"/></a></td>
+							<td><a href="/pics/2003/pinkpanther.jpg"><img src="/pics/2003/pinkpanther_s.jpg" border="0" width="100" height="100"/></a></td>
+							<td><a href="/pics/2003/marc_lounging.jpg"><img src="/pics/2003/marc_lounging_s.jpg" border="0" width="100" height="100"/></a></td>
+						</tr>
+						<tr>
+							<td colSpan="5"><b>*Photos courtesy of Bethany Amos</b><br /><br /></td>
+						</tr>
+
+            <tr>
+              <td colSpan="5"><code><b>Digital photos courtesy of Eric St-Cyr</b></code></td>
+            </tr>
+          </tbody>
+        </table> 
+
+      </div>
+    </div>
+  )
+}
