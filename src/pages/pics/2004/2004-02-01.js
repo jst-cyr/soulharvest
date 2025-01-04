54dfb76957390f1af2416980b68744a6ff5ec1bf
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2025-01-04 08:57:09 -0500

    Adding in 2004-02-01 pictures

---
+++ src/pages/pics/2004/2004-02-01.js
@@ -0,0 +1,77 @@
+import Head from 'next/head';
+import styles from '../../../styles/soulharvest.module.css';
+
+export default function Pics20040201() {
+  return (
+    <div>
+      <Head>
+        <title>Soul Harvest - 2004-Feb-01 - Babylon</title>
+      </Head>
+    
+      <h1>2004-Feb-01 - Babylon</h1>
+      <div className={styles.row}>
+        <table border="0" cellSpacing="0" cellPadding="5"> 
+          <tbody>
+            <tr>
+              <td colspan="5"><h4>Superbowl photos #2</h4></td>
+            </tr>
+            <tr> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_Marc.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_Marc_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_Band.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_Band_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_Ryan_Jay.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_Ryan_Jay_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_Band3.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_Band3_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_Jay.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_Jay_s.jpg" border="0" /></a></td> 
+            </tr> 
+
+            <tr> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_Band2.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_Band2_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_Ryan.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_Ryan_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_Jay2.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_Jay2_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_Chris.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_Chris_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_Marc_Ryan.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_Marc_Ryan_s.jpg" border="0" /></a></td> 
+            </tr> 
+
+            <tr>
+              <td colspan="5"><b>*Photos courtesy of Lisa Kelly</b></td>
+            </tr>
+
+            <tr>
+              <td colspan="5"><hr /><br /><br /></td>
+            </tr>
+            <tr>
+              <td colspan="5"><h4>Superbowl photos #1</h4></td>
+            </tr>
+
+            <tr> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_babylon.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_babylon_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_babylon2.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_babylon2_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_babylon3.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_babylon3_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_babylon4.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_babylon4_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_babylon5.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_babylon5_s.jpg" border="0" /></a></td> 
+            </tr> 
+          
+            <tr> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_babylon6.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_babylon6_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_babylon7.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_babylon7_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_babylon8.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_babylon8_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_babylon9.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_babylon9_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_babylon10.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_babylon10_s.jpg" border="0" /></a></td> 
+            </tr> 
+            <tr> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_babylon11.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_babylon11_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_babylon12.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_babylon12_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_babylon13.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_babylon13_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_babylon14.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_babylon14_s.jpg" border="0" /></a></td> 
+              <td><a href="/pics/2004/2004-02-01/2004-02-01_babylon15.jpg"><img src="/pics/2004/2004-02-01/2004-02-01_babylon15_s.jpg" border="0" /></a></td> 
+            </tr> 
+
+            <tr>
+              <td colspan="5"><b>* Photos courtesy of Eric St-Cyr and <a href="http://www.xfmottawa.com" target="_blank">xfmottawa.com</a></b></td>
+            </tr>
+          </tbody>
+        </table> 
+
+      </div>
+    </div>
+  )
+}
