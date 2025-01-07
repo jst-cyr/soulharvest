266c0ad3821ec5dbb824e00c3626444e1c621a98
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2025-01-07 06:55:03 -0500

    Adding "Rift" recording pics

---
+++ src/pages/pics/rec_rift.js
@@ -0,0 +1,42 @@
+import Head from 'next/head';
+import styles from '../../styles/soulharvest.module.css';
+
+export default function PicsRift() {
+  return (
+    <div>
+      <Head>
+        <title>Soul Harvest - Recording "The Rift"</title>
+      </Head>
+    
+      <h1>Recording "The Rift"</h1>
+      <div className={styles.row}>
+        <table border="0" cellSpacing="0" cellPadding="5"> 
+          <tbody>
+            <tr>
+              <td><a href="/pics/rec_rift/studioband.jpg"><img src="/pics/rec_rift/studioband_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/rec_rift/studioband2.jpg"><img src="/pics/rec_rift/studioband2_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/rec_rift/studioband3.jpg"><img src="/pics/rec_rift/studioband3_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/rec_rift/studioband4.jpg"><img src="/pics/rec_rift/studioband4_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/rec_rift/studioband5.jpg"><img src="/pics/rec_rift/studioband5_s.jpg" border="0" width="100" height="100" /></a></td>
+            </tr>
+            <tr>
+              <td><a href="/pics/rec_rift/studiochris.jpg"><img src="/pics/rec_rift/studiochris_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/rec_rift/studiojay.jpg"><img src="/pics/rec_rift/studiojay_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/rec_rift/studiosean.jpg"><img src="/pics/rec_rift/studiosean_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/rec_rift/studiodave.jpg"><img src="/pics/rec_rift/studiodave_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/rec_rift/studiochris2.jpg"><img src="/pics/rec_rift/studiochris2_s.jpg" border="0" width="100" height="100" /></a></td>
+            </tr>
+            <tr>
+              <td><a href="/pics/rec_rift/studiosean2.jpg"><img src="/pics/rec_rift/studiosean2_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/rec_rift/studioryan.jpg"><img src="/pics/rec_rift/studioryan_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/rec_rift/studiojay2.jpg"><img src="/pics/rec_rift/studiojay2_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td><a href="/pics/rec_rift/studioryan2.jpg"><img src="/pics/rec_rift/studioryan2_s.jpg" border="0" width="100" height="100" /></a></td>
+              <td>&nbsp;</td>
+            </tr>
+          </tbody>
+        </table> 
+
+      </div>
+    </div>
+  )
+}
