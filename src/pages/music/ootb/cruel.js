3f4d0339f00d507d9ccf4c64b41ea1a99109e907
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2025-01-08 06:57:17 -0500

    Adding Cruel song page

---
+++ src/pages/music/ootb/cruel.js
@@ -0,0 +1,74 @@
+import Head from 'next/head';
+import styles from '../../../styles/soulharvest.module.css';
+
+export default function Cruel() {
+  return (
+    <div>
+      <Head>
+        <title>Soul Harvest - Cruel</title>
+      </Head>
+    
+      <h1>Cruel</h1>
+      <div className={styles.row}>
+        <table>
+          <tbody>
+            <tr valign="top">
+              <td width="50%">
+                <font face="arial,helvetica,verdana" size="-1">
+                  <a href="../ootb/"><img src="/music/ootb/ootb.jpg" width="300" /></a>
+                  <br />
+                  <b>Album: </b>Out of the Basement EP<br />
+                  <b>Lyrics by: </b>Jason St-Cyr<br />
+                  <b>Music by: </b>Jason St-Cyr<br />
+                  <b>Performed by: </b>Soul Harvest<br />
+                  <b>Recorded: </b>July 8th, 2000 at BS Trax Studios in Dunrobin, Ontario<br /><br />
+
+
+                  <table border="0">
+                    <tbody>
+                      <tr valign="top">
+                        <td>
+                          <img src="/graphics/spkr.gif" border="0" />
+                        </td>
+                        <td>
+                          <font face="arial,helvetica,verdana" size="-1">Clip [N/A]</font>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <a href="/music/ootb/cruel.mp3"><img src="/graphics/download.gif" height="20" border="0" /></a>
+                        </td>
+                        <td>
+                          <font face="arial,helvetica,verdana" size="-1"><a href="/music/ootb/cruel.mp3">Download</a></font>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <a href="/music/ootb/"><img src="/music/ootb/ootb_s.jpg" height="20" border="0" /></a>
+                        </td>
+                        <td>
+                          <font face="arial,helvetica,verdana" size="-1"><a href="/music/ootb/">CD INFO</a></font>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </font>
+              </td>
+              <td>
+                <font face="arial,helvetica,verdana" size="-1">
+                  <font size="+1"><b>Lyrics and Story</b></font>
+                  <br /><br />
+                  "Darling you've been telling me lies<br />
+                  And honey I've gotta get up and say goodbye<br />
+                  Cause baby you're cruel<br />
+                  You're so mean to me"<br /><br />
+                  Written in 1999 by Sean McCullough and Jason St-Cyr as part of their blues side-project, this song was adopted by Soul Harvest during the great band-merging in January of 2000. The song features Jason St-Cyr on lead guitar, Ryan Linday on rhythm guitar, Kristen Lewis on lead vocals, Chris Amos on drums, Eric St-Cyr on bass guitar and keyboards, and Johnny Arthur on harmonica.  This track is a sample from the newly recorded Soul Harvest demo, and will be on the upcoming SH CD due out by the end of 2000. 
+                </font>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>       
+    </div>
+  )
+}
