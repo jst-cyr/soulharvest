a6bc98a8b3786dc0ff6d6e10db8de3ea8e002ee3
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2025-01-13 22:34:07 -0500

    Adding Midnight Sky to archive

--- src/pages/music/archive.js
+++ src/pages/music/archive.js
@@ -17,113 +17,114 @@ export default function Archive() {
       <div className={styles.row}>
         <center>
         <table width="75%" cols="3" cellpadding="2" cellspacing="0" border="0">
-          <tr bgcolor="#444444">
-            <td align="left" width="50%" valign="top">
-              <font face="arial,helvetica,verdana" color="#0099FF"><b>SONG</b> <font size="-1">(click for more info)</font></font>
-            </td>
-            <td valign="top">
-              <center><font face="arial,helvetica,verdana" color="#0099FF"><b>CLIPS</b></font></center>
-            </td>
-            <td align="left" valign="top">
-              <center><font face="arial,helvetica,verdana" color="#0099FF"><b>GET</b></font></center>
-            </td>
-          </tr>
+          <tbody>
+            <tr bgcolor="#444444">
+              <td align="left" width="50%" valign="top">
+                <font face="arial,helvetica,verdana" color="#0099FF"><b>SONG</b> <font size="-1">(click for more info)</font></font>
+              </td>
+              <td valign="top">
+                <center><font face="arial,helvetica,verdana" color="#0099FF"><b>CLIPS</b></font></center>
+              </td>
+              <td align="left" valign="top">
+                <center><font face="arial,helvetica,verdana" color="#0099FF"><b>GET</b></font></center>
+              </td>
+            </tr>
 
-          {/* BONUS TRAX */}
-          <tr>
-            <td align="center" colspan="3">
-              <img src="/graphics/blackwav.gif" alt="Bonus Trax" border="0" height="50" width="50" align="left" />
-              <font face="arial,helvetica,verdana" color="#0099FF"><b>Bonus Trax</b></font>
-            </td>
-          </tr>
+            {/* BONUS TRAX */}
+            <tr>
+              <td align="center" colspan="3">
+                <img src="/graphics/blackwav.gif" alt="Bonus Trax" border="0" height="50" width="50" align="left" />
+                <font face="arial,helvetica,verdana" color="#0099FF"><b>Bonus Trax</b></font>
+              </td>
+            </tr>
 
-          <tr bgcolor="#444444">
-            <td><font face="arial,helvetica" size="-1" color="#33CCFF">Blame (Live @ CCP)</font></td>
-            <td align="center">&nbsp;</td>
-            <td align="center">
-              <a href="ccp_blame.mp3"><img src="/graphics/download.gif" height="20" border="0" alt="Download" /></a>
-            </td>
-          </tr>
+            <tr bgcolor="#444444">
+              <td><font face="arial,helvetica" size="-1" color="#33CCFF">Blame (Live @ CCP)</font></td>
+              <td align="center">&nbsp;</td>
+              <td align="center">
+                <a href="ccp_blame.mp3"><img src="/graphics/download.gif" height="20" border="0" alt="Download" /></a>
+              </td>
+            </tr>
 
-          <tr>
-            <td><font face="arial,helvetica" size="-1" color="#33CCFF">John Doe (Live @ CCP)</font></td>
-            <td align="center">&nbsp;</td>
-            <td align="center">
-              <a href="ccp_johndoe.mp3"><img src="/graphics/download.gif" height="20" border="0" alt="Download" /></a>
-            </td>
-          </tr>
+            <tr>
+              <td><font face="arial,helvetica" size="-1" color="#33CCFF">John Doe (Live @ CCP)</font></td>
+              <td align="center">&nbsp;</td>
+              <td align="center">
+                <a href="ccp_johndoe.mp3"><img src="/graphics/download.gif" height="20" border="0" alt="Download" /></a>
+              </td>
+            </tr>
 
-          <tr bgcolor="#444444">
-            <td>
-              <a href="midnight_sky.html">
-                <font face="arial,helvetica" size="-1" color="#33CCFF">Midnight Sky</font>
-              </a>
-            </td>
-            <td align="center">&nbsp;</td>
-            <td align="center">
-              <a href="Midnight_Sky.mp3"><img src="/graphics/download.gif" height="20" border="0" alt="Download" /></a>
-            </td>
-          </tr>
+            <tr bgcolor="#444444">
+              <td>
+                <a href="midnight_sky">
+                  <font face="arial,helvetica" size="-1" color="#33CCFF">Midnight Sky</font>
+                </a>
+              </td>
+              <td align="center">&nbsp;</td>
+              <td align="center">
+                <a href="Midnight_Sky.mp3"><img src="/graphics/download.gif" height="20" border="0" alt="Download" /></a>
+              </td>
+            </tr>
 
-          <tr>
-            <td>
-              <a href="mirrortime.html">
-                <font face="arial,helvetica" size="-1" color="#33CCFF">Mirrortime</font>
-              </a>
-            </td>
-            <td align="center">&nbsp;</td>
-            <td align="center">
-              <a href="Mirrortime.mp3"><img src="/graphics/download.gif" height="20" border="0" alt="Download" /></a>
-            </td>
-          </tr>
+            <tr>
+              <td>
+                <a href="mirrortime.html">
+                  <font face="arial,helvetica" size="-1" color="#33CCFF">Mirrortime</font>
+                </a>
+              </td>
+              <td align="center">&nbsp;</td>
+              <td align="center">
+                <a href="Mirrortime.mp3"><img src="/graphics/download.gif" height="20" border="0" alt="Download" /></a>
+              </td>
+            </tr>
 
-          <tr bgcolor="#444444">
-            <td>
-              <font face="arial,helvetica" size="-1" color="#33CCFF">Something Soothing (Live @ CCP)</font>
-            </td>
-            <td align="center">&nbsp;</td>
-            <td align="center">
-              <a href="ccp_somethingsoothing.mp3"><img src="/graphics/download.gif" height="20" border="0" alt="Download" /></a>
-            </td>
-          </tr>
+            <tr bgcolor="#444444">
+              <td>
+                <font face="arial,helvetica" size="-1" color="#33CCFF">Something Soothing (Live @ CCP)</font>
+              </td>
+              <td align="center">&nbsp;</td>
+              <td align="center">
+                <a href="ccp_somethingsoothing.mp3"><img src="/graphics/download.gif" height="20" border="0" alt="Download" /></a>
+              </td>
+            </tr>
 
-          {/* OUT OF THE BASEMENT */}
-          <tr align="middle">
-            <td align="center" colspan="3">
-              <hr />
-              <a href="ootb/">
-                <img src="ootb/ootb_s.jpg" alt="Out of the Basement" border="0" height="50" width="50" align="left" />
-              </a>
-              <font face="arial,helvetica,verdana" color="#0099FF"><b>Out of the Basement EP</b></font>
-            </td>
-          </tr>
+            {/* OUT OF THE BASEMENT */}
+            <tr align="middle">
+              <td align="center" colspan="3">
+                <hr />
+                <a href="ootb/">
+                  <img src="ootb/ootb_s.jpg" alt="Out of the Basement" border="0" height="50" width="50" align="left" />
+                </a>
+                <font face="arial,helvetica,verdana" color="#0099FF"><b>Out of the Basement EP</b></font>
+              </td>
+            </tr>
 
-          <tr bgcolor="#444444">
-            <td>
-              <a href="ootb/chilling.html">
-                <font face="arial,helvetica" color="#33CCFF" size="-1">Chillin' on the Patio</font>
-              </a>
-            </td>
-            <td align="center">&nbsp;</td>
-            <td align="center">
-              <a href="ootb/chilling.mp3"><img src="/graphics/download.gif" height="20" border="0" alt="Download" /></a>
-            </td>
-          </tr>
+            <tr bgcolor="#444444">
+              <td>
+                <a href="ootb/chilling.html">
+                  <font face="arial,helvetica" color="#33CCFF" size="-1">Chillin' on the Patio</font>
+                </a>
+              </td>
+              <td align="center">&nbsp;</td>
+              <td align="center">
+                <a href="ootb/chilling.mp3"><img src="/graphics/download.gif" height="20" border="0" alt="Download" /></a>
+              </td>
+            </tr>
 
-          <tr>
-            <td>
-              <a href="ootb/cruel.html">
-                <font face="arial,helvetica" size="-1" color="#33CCFF">Cruel</font>
-              </a>
-            </td>
-            <td align="center">&nbsp;</td>
-            <td align="center">
-              <a href="ootb/cruel.mp3"><img src="/graphics/download.gif" height="20" border="0" alt="Download" /></a>
-            </td>
-          </tr>
+            <tr>
+              <td>
+                <a href="ootb/cruel.html">
+                  <font face="arial,helvetica" size="-1" color="#33CCFF">Cruel</font>
+                </a>
+              </td>
+              <td align="center">&nbsp;</td>
+              <td align="center">
+                <a href="ootb/cruel.mp3"><img src="/graphics/download.gif" height="20" border="0" alt="Download" /></a>
+              </td>
+            </tr>
 
-          {/*Additional rows have been omitted for brevity*/}
-
+            {/*Additional rows have been omitted for brevity*/}
+          </tbody>
         </table>
 
         </center>
---
+++ src/pages/music/midnight_sky.js
@@ -0,0 +1,121 @@
+import Head from 'next/head';
+import styles from '../../styles/soulharvest.module.css';
+
+export default function MidnightSky() {
+  return (
+    <div>
+      <Head>
+        <title>Soul Harvest - Midnight Sky</title>
+      </Head>
+    
+      <h1>Midnight Sky</h1>
+      <div className={styles.row}>
+        <table>
+          <tbody>
+            <tr valign="top">
+              <td width="50%">
+                <font face="arial,helvetica,verdana" size="-1">
+                  <img src="/graphics/blackwav.gif" width="150" height="150" />
+                  <br />
+                  <b>Lyrics by: </b>Johnny Arthur<br />
+                  <b>Music by: </b>Jay James<br />
+                  <b>Performed by: </b>Jay James<br />
+                  <b>Recorded: </b>April 2000 in Orleans, Ontario<br /><br />
+
+
+                  <table border="0">
+                    <tbody>
+                      <tr valign="top">
+                        <td>
+                          <img src="/graphics/spkr.gif" border="0" />
+                        </td>
+                        <td>
+                          <font face="arial,helvetica,verdana" size="-1">Clip [N/A]</font>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <a href="/music/midnight_sky.mp3"><img src="/graphics/download.gif" height="20" border="0" /></a>
+                        </td>
+                        <td>
+                          <font face="arial,helvetica,verdana" size="-1"><a href="/music/midnight_sky.mp3">Download</a></font>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </font>
+              </td>
+              <td>
+                <font face="arial,helvetica,verdana" size="-1">
+                  <font size="+1"><b>Lyrics</b></font>
+                  <br /><br />
+                  <b>Verse 1:</b>
+                  <br />
+                  You always tried to protect me <br />
+                  From the harm I did myself<br />
+                  You were the best thing that's happened<br />
+                  To this poor shattered shell<br />
+                  <br />
+                  You looked after me so long and hard <br />
+                  I never saw<br />
+                  How much deep damage and hurt<br />
+                  All my failures caused<br />
+                  <br />
+                  <b>Chorus 1:</b>
+                  <br />
+                  So I'll walk alone down this road<br />
+                  If only rid you of my heavy load<br />
+                  So please darlin' just pass me by<br />
+                  Just drive on, drive on<br />
+                  To that Midnight Sky<br />
+                  <br />
+                  <b>Verse 2:</b>
+                  <br />
+                  Though it kills me so to save you<br />
+                  For all your pain I atone<br />
+                  At least I know I won't have<br />
+                  To go on hurting alone<br />
+                  <br />
+                  I pray that when all your tears are wept<br />
+                  You'll try to see<br />
+                  That the last thing you'll ever need<br />
+                  Is a bum like me<br />
+                  <br />
+                  <b>Chorus 2:</b>
+                  <br />
+                  Though maybe baby you want me found<br />
+                  I'd be damned if I dragged you down<br />
+                  So please darlin' just pass me by<br />
+                  Just drive on, drive on<br />
+                  To that Midnight Sky<br />
+                  <br />
+                  <b>Verse 3:</b>
+                  <br />
+                  I decided once and for all<br />
+                  To protect you with a lie<br />
+                  And baby you can't change it<br />
+                  No matter how hard you cry<br />
+                  <br />
+                  You'll never know how much I miss<br />
+                  The sound of your voice<br />
+                  Darling please try to understand<br />
+                  That I had no choice<br />
+                  <br />
+                  <b>Chorus 3:</b>
+                  <br />
+                  I would gladly pay a deathly toll<br />
+                  If that's what it takes to save your soul<br />
+                  So please darlin' just pass me by<br />
+                  Just drive on, drive on<br />
+                  To that Midnight Sky<br />
+                  <br />
+
+                </font>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>       
+    </div>
+  )
+}
