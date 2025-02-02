a63b8db979fadf1c56d09c44d011324536de62bf
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2025-02-01 22:43:10 -0500

    Initial version of 2000 MacFEST listing.
    Still requires fixes for links on this page.

---
+++ src/pages/macfest/2000.js
@@ -0,0 +1,106 @@
+import Head from 'next/head';
+import styles from '../../styles/soulharvest.module.css';
+
+export default function MacFEST2000() {
+  return (
+    <div>
+      <Head>
+        <title>Soul Harvest - MacFEST '00</title>
+      </Head>
+
+      <table>
+        <tbody>
+          <tr valign="top">
+            <td className="soulharvest" width="50%">
+              <img src="macfest_m.jpg" border="0" alt="MacFEST 2000" />
+            </td>
+            <td className="soulharvest">
+              <font face="arial,helvetica,verdana" size="-1">
+                <br /><br />
+                <b>When: </b>All-day/night Sunday, August 6th, 2000<br />
+                <b>Where: </b>Molly's (formerly Molly McGuire's) in the Ottawa market<br />
+                <b>Address: </b>130 George St., Ottawa, ON, Canada<br />
+                <b>Why: </b>All proceeds will be donated to the Bear Children's Fund<br />
+                <b>How much: </b>$5 at the door for the whole day<br /><br />
+                <font size="+1"><b>What's the deal?</b></font>
+                <br /><br />
+                Soul Harvest is putting together a benefit concert for the Bear Children's Fund. Set for August 6th at Molly's (formerly Molly McGuire's) in the market, here's the showtimes!
+                <center>
+                  <table border="0" cellpadding="5" cellspacing="5">
+                    <tr valign="top">
+                      <td align="right">
+                        <font face="arial,helvetica,verdana" size="-1"><b>12:00pm</b></font>
+                      </td>
+                      <td align="left">
+                        <font face="arial,helvetica,verdana" size="-1">Soul Harvest</font>
+                      </td>
+                    </tr>
+                    <tr valign="top">
+                      <td align="right">
+                        <font face="arial,helvetica,verdana" size="-1"><b>12:30pm</b></font>
+                      </td>
+                      <td align="left">
+                        <font face="arial,helvetica,verdana" size="-1"><a href="http://artists.mp3s.com/artists/102/veronica_charnley.html" target="_new">"Veronica Charnley"</a></font>
+                      </td>
+                    </tr>
+                    <tr valign="top">
+                      <td align="right">
+                        <font face="arial,helvetica,verdana" size="-1"><b>2:30pm</b></font>
+                      </td>
+                      <td align="left">
+                        <font face="arial,helvetica,verdana" size="-1"><a href="http://www.mp3.com/aitimusic" target="_new">"aiti maa"</a></font>
+                      </td>
+                    </tr>
+                    <tr valign="top">
+                      <td align="right">
+                        <font face="arial,helvetica,verdana" size="-1"><b>4:30pm</b></font>
+                      </td>
+                      <td align="left">
+                        <font face="arial,helvetica,verdana" size="-1"><a href="http://www.mp3.com/shunt" target="_new">"Shunt"</a></font>
+                      </td>
+                    </tr>
+                    <tr valign="top">
+                      <td align="right">
+                        <font face="arial,helvetica,verdana" size="-1"><b>6:30pm</b></font>
+                      </td>
+                      <td align="left">
+                        <font face="arial,helvetica,verdana" size="-1"><a href="http://www.mp3.com/tos" target="_new">"Tables of Stone"</a></font>
+                      </td>
+                    </tr>
+                    <tr valign="top">
+                      <td align="right">
+                        <font face="arial,helvetica,verdana" size="-1"><b>8:30pm</b></font>
+                      </td>
+                      <td align="left">
+                        <font face="arial,helvetica,verdana" size="-1"><a href="http://www.mp3.com/ignition" target="_new">"Ignition"</a></font>
+                      </td>
+                    </tr>
+                    <tr valign="top">
+                      <td align="right">
+                        <font face="arial,helvetica,verdana" size="-1"><b>10:30pm</b></font>
+                      </td>
+                      <td align="left">
+                        <font face="arial,helvetica,verdana" size="-1"><a href="http://www.mp3.com/divinityburst" target="_new">"Divinity Burst"</a></font>
+                      </td>
+                    </tr>
+                    <tr valign="top">
+                      <td align="right">
+                        <font face="arial,helvetica,verdana" size="-1"><b>12:30am</b></font>
+                      </td>
+                      <td align="left">
+                        <font face="arial,helvetica,verdana" size="-1">Soul Harvest</font>
+                      </td>
+                    </tr>
+                  </table>
+                </center>
+                <br />
+                So why not come check it out! Access to the all-day/all-night event will be only $5 at the door!! <br /><br />
+                More information will be announced in the coming weeks, so keep coming back to find out more or drop us a line and join Soul Harvest's mailing list!
+              </font>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
